96f5652a08745ff39eec943b3db1e8b446f8aa90
Body811 <bodyashraf500@gmail.com>
2025-02-22 18:22:57 +0200

    updated pydantic models

--- app/api/v1/models/image_similarity.py
+++ app/api/v1/models/image_similarity.py
@@ -1,17 +1,29 @@
-from pydantic import BaseModel
-from typing import List
+from pydantic import BaseModel, Field
+from typing import List, Dict
 
 class StoreImageRequest(BaseModel):
-    url: List[str]
+    data: Dict[int, str] = Field(
+        ...,
+        examples=[{
+            1: "https://example.com/image1.jpg",
+            2: "https://example.com/image2.jpg",
+            3: "https://example.com/image3.jpg"
+            }]
+        )
     
 class StoreImageResponse(BaseModel):
-    images_stored: int
-    message: str
+    images_stored: int = Field(...,examples=[3])
+    message: str = Field(..., examples=["Images Stored Successfully"])
     
-class getImageSimilarityRequest(BaseModel):
+class ImageSimilarityRequest(BaseModel):
     url: str
-    topk: int
+    top_k: int
     
-class getImageSimilarityResponse(BaseModel):
+class ImageSimilarityResponse(BaseModel):
     products: List[str]
+
+class deleteImageRequest(BaseModel):
+    id: int
     
+class deleteImageResponse(BaseModel):
+    message: str
